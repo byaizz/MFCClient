503baa0ffcb82cf36a2e540db2afb8af2fbd1a7c
bys <410389821@qq.com>
2018-12-06 14:30:44 +0800

    excel操作类saveas函数增加异常判断

--- PRAS_Client/ExcelManager.h
+++ PRAS_Client/ExcelManager.h
@@ -26,8 +26,8 @@ public:
 	bool Open(const CString &iFileName, bool autoCreate = false);//打开文件,默认文件不存在不自动创建
 	bool OpenFromTemplate(const CString &iFileName);//打开以模板新建文件
 	void Save();//OpenFromTemplate创建的文件不能使用该方法保存
-	void SaveAs(const CString &iFileName);
-	void SaveAsPDF(const CString &iFileName);
+	bool SaveAs(const CString &iFileName);
+	bool SaveAsPDF(const CString &iFileName);
 	void Test();
 private:
 	CApplication	app;
